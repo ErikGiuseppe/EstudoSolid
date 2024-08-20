ac7308d6c2f21008a7c866b61bbe402274baf809
Erik Giuseppe Kato Bandeira <erik.setide@osasco.sp.gov.br>
2024-08-20 11:30:08 -0300

    subindo a quinta demanda

---
+++ tarefa-5/repositories/interfaces/IClienteRepository.ts
@@ -0,0 +1,7 @@
+import Cliente from "../../entities/Cliente"
+
+interface IClienteRepository {
+    adicionaCliente(cliente: Cliente): void
+    listaClientes(): Cliente[]
+}
+export default IClienteRepository
